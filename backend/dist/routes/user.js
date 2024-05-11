c1530ce1bfb52ea9c3aa50c4001e6fa6043a4931
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-11 16:01:38 -0400

    git commit -m "Remove dist directory from version control"

--- backend/dist/routes/user.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
-import { validateSignUp } from "../middlewares/index.js";
-const userRoutes = Router();
-// /user/
-userRoutes.get('/', getAllUsers);
-userRoutes.post('/signup', validateSignUp, userSignup);
-export default userRoutes;
