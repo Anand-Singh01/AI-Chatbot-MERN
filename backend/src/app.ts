84ec6eb326a05807b0c31ab71c2339afac5c64e5
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-30 16:21:25 -0400

    test3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 //middleware
 app.use(cors({
-    origin:"http://localhost:5173/",
+    origin:"http://localhost:5173",
     credentials:true
 }));
 app.use(express.json());
