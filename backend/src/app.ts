9d3078a83660439900ca405705d0cc03512bea74
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-30 16:16:52 -0400

    origin changed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,11 +9,7 @@ const app = express();
 
 //middleware
 app.use(cors({
-<<<<<<< HEAD
     origin:"http://localhost:5173/",
-=======
-    origin:'*',
->>>>>>> 5674991a1323373066913b6f6652eae6ac07ca3a
     credentials:true
 }));
 app.use(express.json());
