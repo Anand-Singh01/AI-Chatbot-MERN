c6ca3354a81a02ad0f6715b25e60db8311d4c7c6
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-30 22:25:51 -0400

    cors origin changed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 //middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL1!, process.env.CLIENT_URL2!],
     credentials:true
 }));
 app.use(express.json());
