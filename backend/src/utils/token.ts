d31b0074b1f63ab62e76628475542083ab89bd4b
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-30 21:08:49 -0400

    domain nam removed

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -3,36 +3,46 @@ import jwt from "jsonwebtoken";
 import { COOKIE_NAME } from "./constants.js";
 
 export const createToken = (id: string, email: string, expiresIn: string) => {
-    const payload = { id, email };
-    const token = jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn:expiresIn });
-    return token;
+  const payload = { id, email };
+  const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+    expiresIn: expiresIn,
+  });
+  return token;
 };
 
-export const handleTokenAndCookie = (res : Response, id : string, email : string, expiresIn: string)=>
-{
-    res.clearCookie(COOKIE_NAME, {path:"/", domain:"netlify.app", httpOnly:true, signed:true});
-    const token = createToken(id, email, expiresIn);
-    const expires = new Date();
-    expires.setDate(expires.getDate() + 7);
-    res.cookie(COOKIE_NAME, token, {path:"/", domain:"netlify.app", expires, httpOnly:true, signed:true});
-}
+export const handleTokenAndCookie = (
+  res: Response,
+  id: string,
+  email: string,
+  expiresIn: string
+) => {
+  res.clearCookie(COOKIE_NAME, { path: "/", httpOnly: true, signed: true });
+  const token = createToken(id, email, expiresIn);
+  const expires = new Date();
+  expires.setDate(expires.getDate() + 7);
+  res.cookie(COOKIE_NAME, token, {
+    path: "/",
+    expires,
+    httpOnly: true,
+    signed: true,
+  });
+};
 
-export const verifyToken = (req: Request, res: Response, next:NextFunction)=>
-{
-    try
-    {
-        const token = req.signedCookies[COOKIE_NAME];
-        if(!token || token.trim() == '')
-        {
-            return res.status(401).json({message:"Session expired"});
-        }
-        const data = jwt.verify(token, process.env.JWT_SECRET!);
-        res.locals.jwtData = data;
-        next();
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.signedCookies[COOKIE_NAME];
+    if (!token || token.trim() == "") {
+      return res.status(401).json({ message: "Session expired" });
     }
-    catch(error)
-    {
-        console.log(error);
-        return res.status(401).json({message:"Session expired"});
-    }
-}
+    const data = jwt.verify(token, process.env.JWT_SECRET!);
+    res.locals.jwtData = data;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Session expired" });
+  }
+};
